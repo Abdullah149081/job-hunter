9229be4275a5a31139d7bfa831108805f059af7b
Abdullah149081 <masudmd425@gmail.com>
2023-04-10 16:46:18 +0600

    featured jobs done

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+
+const FeaturedJobs = ({ job }) => {
+  const { company_logo, job_title, company_name, remote_or_onsite, fulltime_or_parttime, salary, location } = job;
+  return (
+    <div className="border rounded-sm py-10 pl-10">
+      <img className="w-64  h-20 object-cover" src={company_logo} alt="" />
+      <div className="space-y-5 mt-8">
+        <h2 className="text-2xl font-bold text-neutral-700">{job_title}</h2>
+        <p className="text-lg font-bold text-slate-500">{company_name}</p>
+        <p className="font-semibold">
+          <span className="remote_or_fulltime text-g ">{remote_or_onsite}</span> <span className=" remote_or_fulltime text-g">{fulltime_or_parttime}</span>
+        </p>
+        <p className="text-lg font-bold text-stone-600 flex  gap-8 items-center">
+          <span className="location_salary">
+            <HiOutlineLocationMarker />
+            {location}
+          </span>
+          <span className="location_salary">Salary : {salary}</span>
+        </p>
+      <button className="btn font-bold">View Details</button>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedJobs;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Lottie from "lottie-react";
 import { useLoaderData } from "react-router-dom";
 import reader from "../../assets/129467-job-hunting.json";
 import Category from "../Category/Category";
+import FeaturedJobs from "../FeaturedJobs/FeaturedJobs";
 
 const Home = () => {
   const category = useLoaderData();
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
+
   return (
     <div className="px-4 py-16  md:px-24 lg:px-28 lg:py-20">
+      {/* banner  */}
       <div className="flex flex-col md:flex-row items-center justify-between ">
         <div className="lg:w-1/2 space-y-8 ">
           <h1 className="text-4xl md:text-7xl font-bold lg:leading-tight ">
-            One Step Closer To Your <span className="bg-gradient-to-r from-indigo-500 to-violet-500 text-transparent  bg-clip-text">Dream Job</span>
+            One Step Closer To Your <span className="text-g ">Dream Job</span>
           </h1>
           <p className="text-stone-500 lg:text-2xl">
             Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
@@ -25,12 +34,23 @@ const Home = () => {
           </div>
         </div>
       </div>
+      {/* job category  */}
       <div className="mt-32">
         <h2 className="text-center text-5xl font-bold mb-5">Job Category List</h2>
         <p className="text-center text-stone-500">Explore thousands of job opportunities with all the information you need. Its your future</p>
         <div className="grid lg:grid-cols-4 mt-32 gap-6">
           {category.map((list, index) => (
             <Category key={index} list={list}></Category>
+          ))}
+        </div>
+      </div>
+      {/* Featured Jobs */}
+      <div className="mt-32">
+        <h2 className="text-center text-5xl font-bold mb-5">Featured Jobs</h2>
+        <p className="text-center text-stone-500">Explore thousands of job opportunities with all the information you need. Its your future</p>
+        <div className="grid lg:grid-cols-2 mt-32 gap-6">
+          {jobs.map((job) => (
+            <FeaturedJobs key={job.id} job={job}></FeaturedJobs>
           ))}
         </div>
       </div>
