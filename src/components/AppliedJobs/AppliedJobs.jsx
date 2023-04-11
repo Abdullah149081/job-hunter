b8ad2d20ad2683073c7379d3b6554b435ad10409
Abdullah149081 <masudmd425@gmail.com>
2023-04-11 17:40:04 +0600

    udpate applied jobs page

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -20,14 +20,16 @@ const AppliedJobs = () => {
     <div className="px-4 py-16  md:px-24 lg:px-28 lg:py-20">
       <p className="text-center text-4xl font-bold text-gray-800">{applies.length > 0 ? <span>Applied Jobs</span> : <span>You're not applied any job</span>}</p>
       <div className="lg:mt-36 mt-24  lg:px-40 ">
-        <div className="space-x-4 font-bold lg:text-right text-center">
-          <button onClick={handlerRemote} className="btn">
-            Remote
-          </button>
-          <button onClick={handlerOnsite} className="btn">
-            On-site
-          </button>
-        </div>
+        {applies.length > 0 && (
+          <div className="space-x-4 font-bold lg:text-right text-center">
+            <button onClick={handlerRemote} className="btn">
+              Remote
+            </button>
+            <button onClick={handlerOnsite} className="btn">
+              On-site
+            </button>
+          </div>
+        )}
         <div>
           {applies.map((applied) => (
             <AppliedDetails key={applied.id} applied={applied}></AppliedDetails>
