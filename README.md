07a94761864484caa26c541936798f7ee2c355f5
Abdullah149081 <masudmd425@gmail.com>
2023-04-12 12:36:33 +0600

    What is useRef Q done

--- README.md
+++ README.md
@@ -3,29 +3,25 @@
 ## Features
 
 - Home Page
-
- * Job Category list fetch api then show ui
- * same Work Featured Jobs
+  * Job Category list fetch api then show ui
+  * same Work Featured Jobs
 
 - Statistics Page
-
- * show Assignment marks 1-8
- * Recharts use SimpleAreaChart
+  * show Assignment marks 1-8
+  * Recharts use SimpleAreaChart
 
 - Applied Jobs Page
+  * job Details apply now btn click set Data local Storage
+  * show data applied jobs page 
+  * remote btn click show remote job
+  * onSite btn click show on Site job
 
- * job Details apply now btn click set Data local Storage
- * show data applied jobs page 
- * remote btn click show remote job
- * onSite btn click show on Site job
-
--Blog Page
-
- * Answer  4 Question 
- 1. When should you use context API?
- 2. What is a custom hook?
- 3. What is useRef?
- 4. What is useMemo?
+- Blog Page
+  * Answer  4 Question 
+    1. When should you use context API?
+    2. What is a custom hook?
+    3. What is useRef?
+    4. What is useMemo?
 
 
 ## Live Link
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -6,7 +6,7 @@ const Blog = () => {
       <h2 className="text-center text-4xl font-bold text-gray-900">Question and Answer</h2>
       <div class="grid lg:grid-cols-3 gap-6 mt-10">
         <div class="space-y-4 border-2 p-4 border-dotted rounded-lg border-indigo-500">
-          <h2 class="lg:text-2xl font-bold">1. When should you use context API?</h2>
+          <h2 class="lg:text-2xl font-bold">1. When should you use context API ?</h2>
           <p class="text-gray-700 font-medium lg:text-xl">
             The Context API is primarily used when some data needs to be accessible by many components at different nesting levels. This is the alternative to "prop drilling" or moving props from
             grandparent to child to parent.
@@ -14,10 +14,18 @@ const Blog = () => {
         </div>
         {/* Q-2 */}
         <div class="space-y-4 border-2 p-4 border-dotted rounded-lg border-indigo-500">
-          <h2 class="lg:text-2xl font-bold">2. What is a custom hook??</h2>
+          <h2 class="lg:text-2xl font-bold">2. What is a custom hook ?</h2>
           <p class="text-gray-700 font-medium lg:text-xl">
             As a result of using custom hook, we can reuse the code, which increases the readability of the code, as a result, many complex problems can be solved, when the same work or same code
             needs to be written repeatedly, we can solve this problem by using custom hook. (use) should be written at the beginning.
+          </p>
+        </div>
+        {/* Q-3 */}
+        <div class="space-y-4 border-2 p-4 border-dotted rounded-lg border-indigo-500">
+          <h2 class="lg:text-2xl font-bold">3. What is useRef ?</h2>
+          <p class="text-gray-700 font-medium lg:text-xl">
+            useRef is a React Hook that lets you reference a value that’s not needed for rendering.useRef returns an object with a single property.By using this we can access and manipulate DOM
+            elements directly.Implementing a stopwatch,focus on an input.
           </p>
         </div>
       </div>
